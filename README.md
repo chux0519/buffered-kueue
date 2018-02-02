d6af7b256944d43c047d716ced5654bab5eae41c
Yongsheng Xu <chuxdesign@hotmail.com>
2018-02-02 17:14:41 +0800

    finish

---
+++ README.md
@@ -0,0 +1,23 @@
+# Buffered Kueue
+
+[![Build Status](https://travis-ci.org/chux0519/buffered-kueue.svg?branch=master)](https://travis-ci.org/chux0519/buffered-kueue)
+[![codecov](https://codecov.io/gh/chux0519/buffered-kueue/branch/master/graph/badge.svg)](https://codecov.io/gh/chux0519/buffered-kueue)
+
+Help you to buffer your tasks
+
+## Features
+
+- Having lifecycle hooks
+- Flushing specific amount of items when interval reached
+
+## Install
+
+> npm install --save buffered-kueue
+>
+> yarn add buffered-kueue
+
+## Intro
+
+## Quick Start
+
+## API
---
+++ test/lib/buffered-kueue.spec.js
@@ -0,0 +1,56 @@
+const Queue = require('../../index')
+
+describe('BufferedQueue', () => {
+  describe('Given no params', () => {
+    const queue = new Queue()
+    it('Should not be started', () => {
+      expect(queue.started).toBe(false)
+    })
+    it('Should stopped after calling start at first flushing', () => {
+      queue.start()
+      expect(queue.started).toBe(true)
+      return sleep(1000).then(() => {
+        expect(expect(queue.started).toBe(false))
+      })
+    })
+  })
+  describe('Given onFlush function, interval to 1000ms, flush size to 1', () => {
+    let result = null
+    const queue = new Queue({onFlush: (items) => { result = items }, flushInterval: 1000, flushSize: 1})
+    it('Should flush every 1000 ms', () => {
+      expect(queue.started).toBe(false)
+      return sleep(1000)
+      .then(() => expect(result).toBe(null))
+      .then(() => {
+        queue.add(1)
+        queue.add(2)
+        expect(queue.started).toBe(true)
+        return sleep(1000)
+      })
+      .then(() => expect(result).toEqual([1]))
+      .then(() => sleep(1000))
+      .then(() => expect(result).toEqual([2]))
+      .then(() => sleep(1000))
+      .then(() => expect(queue.started).toBe(false))
+    })
+  })
+  describe('Given max size to 1, interval to 1000ms', () => {
+    let result = null
+    const queue = new Queue({onFlush: (items) => { result = items }, flushInterval: 1000, maxSize: 1})
+    it('Should flush every 1000 ms', () => {
+      return sleep(1000)
+      .then(() => expect(result).toBe(null))
+      .then(() => {
+        queue.add(1)
+        queue.add(2)
+        queue.add(3)
+        return sleep(1000)
+      })
+      .catch(e => expect(e.message).toMatch(/Queue length exceeds limit/))
+    })
+  })
+})
+
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
