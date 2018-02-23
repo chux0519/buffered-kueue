3aef5b69bf745fe964dae0cf0eb41a3a4078e7c4
Yongsheng Xu <chuxdesign@hotmail.com>
2018-02-23 13:37:47 +0800

    add setters for lifecycle events upgrade version

--- test/lib/buffered-kueue.spec.js
+++ test/lib/buffered-kueue.spec.js
@@ -37,8 +37,8 @@ describe('BufferedQueue', () => {
   })
   describe('Given max size to 1, interval to 1000ms', () => {
     let result = null
-    const queue = new Queue({onFlush: (items) => { result = items }, flushInterval: 1000, maxSize: 1})
-    it('Should flush every 1000 ms', () => {
+    const queue = new Queue({onFlush: (items) => { result = items }, flushInterval: 1000, maxSize: 1, flushSize: 1})
+    it('Should throws when max size setted to 1', () => {
       return sleep(1000)
       .then(() => expect(result).toBe(null))
       .then(() => {
@@ -48,6 +48,19 @@ describe('BufferedQueue', () => {
         return sleep(1000)
       })
       .catch(e => expect(e.message).toMatch(/BufferedQueue length exceeds limit/))
+    })
+    it('Should runs well after setting didPush lifecycle event', () => {
+      queue.didPush = (item, q) => {
+        if (q.length >= queue.flushSize) queue.flush()
+      }
+      return sleep(1000)
+      .then(() => {
+        queue.push(1)
+        queue.push(2)
+        queue.push(3)
+        return sleep(1000)
+      })
+      .then(() => expect(result).toEqual([3]))
     })
   })
 })
